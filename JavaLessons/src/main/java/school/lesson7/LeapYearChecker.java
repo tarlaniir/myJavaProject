a641353b387aebfb372f1c3164fd0fbd641e5b0a
tarlaniir <anahim1996@gmail.com>
2025-01-29 01:29:41 +0400

    lesson7

---
+++ JavaLessons/src/main/java/school/lesson7/LeapYearChecker.java
@@ -0,0 +1,7 @@
+package school.lesson7;
+
+public class LeapYearChecker {
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
